68247216d04c7c5e3c5df05a58f801e5ee8ac5fd
Prashant Temburwar <80477150+prashanttemburwar1999@users.noreply.github.com>
2024-11-28 18:33:22 +0530

    Updated start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,6 +3,5 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull prashanttemburwar1999/my-first-python-cicd
-
 # Run the Docker image as a container
 docker run -d -p 5000:3000 prashanttemburwar1999/my-first-python-cicd
